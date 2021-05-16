5d1c89b71b7a36ca089e0d6acdfa22addcf94262
ven1k <venik.butenko@gmail.com>
2021-05-16 13:56:25 +0300

    16.05

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(function(){
+$(document).ready(function() {
+
+    $("#owl-example").owlCarousel({
+
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    }) ;
+
+        var owlIntro = $("#owl-example_intro");
+
+    owlIntro.owlCarousel({
+            slideSpeed : 300,
+            paginationSpeed : 400,
+            singleItem:true
+        });
+
+
+        // Custom Navigation Events
+        $(".next").click(function(){
+            owlIntro.trigger('owl.next');
+        })
+        $(".prev").click(function(){
+            owlIntro.trigger('owl.prev');
+        })
 
 });
